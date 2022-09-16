a34f6a08db9504c8bfa613cabb61567f6da093ea
T4UA <tauaandrade95@gmail.com>
2022-09-16 00:14:38 -0300

    versão 1.1

--- exercicio_2.py
+++ exercicio_2.py
@@ -1,11 +1,23 @@
-idade = int(input('Qual a sua idade? '))
-if idade > 150 or idade < 0:
-	print('ERRO! Informação invalida')
+while True:
+	idade = int(input('Qual a sua idade? '))
+	if 0 < idade < 150:
+		print('Proxima pergunta')
+		break
+	else:
+		print('ERRO! Informação invalida')
 
-salario = float(input('Informe o seu salario: '))
-if salario < 0:
-	print('ERRO! Informação invalida')
+while True:
+	salario = float(input('Informe o seu salario: R$'))
+	if salario > 0:
+		print('Proxima pergunta')
+		break
+	else:
+		print('ERRO! Informação invalida')
 
-sexo = input('Informe seu sexo M ou F: ').upper()
-if 'M' != sexo != 'F':
-	print('ERRO! Informação invalida')
+while True:
+	sexo = input('Informe seu sexo M ou F: ').strip().upper()[0]
+	if 'M' != sexo != 'F':
+		print('ERRO! Informação invalida')
+	else:
+		print('Projeto finalizado')
+		break
--- exercicio_3.py
+++ exercicio_3.py
@@ -1,18 +1,18 @@
 count = 0
 
-perg_1 = input('Mora perto da vitima? S OU N: ').upper()[0].strip()
+perg_1 = input('Mora perto da vitima? S OU N: ').strip().upper()[0]
 if perg_1 == 'S':
 	count += 1
-perg_2 = input('Já trabalhou com a vitima? S ou N: ').upper()[0].strip()
+perg_2 = input('Já trabalhou com a vitima? S ou N: ').strip().upper()[0]
 if perg_2 == 'S':
 	count += 1
-perg_3 = input('Telefonou para a vitima? S ou N: ').upper()[0].strip()
+perg_3 = input('Telefonou para a vitima? S ou N: ').strip().upper()[0]
 if perg_3 == 'S':
 	count += 1
-perg_4 = input('Esteve no local do crime? S ou N: ').upper()[0].strip()
+perg_4 = input('Esteve no local do crime? S ou N: ').strip().upper()[0]
 if perg_4 == 'S':
 	count += 1
-perg_5 = input('Devia para a vitima? S ou N: ').upper()[0].strip()
+perg_5 = input('Devia para a vitima? S ou N: ').strip().upper()[0]
 if perg_5 == 'S':
 	count += 1
 
